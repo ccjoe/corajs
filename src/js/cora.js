495a3a6b5607367c5f71e0a7ec503071d9bd9438
ccjoe <icareu.joe@gmail.com>
2015-11-07 20:43:21 +0800

    base localstorage api

--- src/js/cora.js
+++ src/js/cora.js
@@ -508,6 +508,148 @@
         };
         return log;
     })();
+    //localStorage Shim(support < ie8)
+    if (!window.localStorage) {
+        Cora.warn('不支持localStorage, 正在使用Cookie存储, 但API兼容localStorage');
+        window.localStorage = {
+            getItem: function (sKey) {
+                if (!sKey || !this.hasOwnProperty(sKey)) { return null; }
+                return unescape(document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1"));
+            },
+            key: function (nKeyId) {
+                return unescape(document.cookie.replace(/\s*\=(?:.(?!;))*$/, "").split(/\s*\=(?:[^;](?!;))*[^;]?;\s*/)[nKeyId]);
+            },
+            setItem: function (sKey, sValue) {
+                if(!sKey) { return; }
+                document.cookie = escape(sKey) + "=" + escape(sValue) + "; expires=Tue, 19 Jan 2038 03:14:07 GMT; path=/";
+                this.length = document.cookie.match(/\=/g).length;
+            },
+            length: 0,
+            removeItem: function (sKey) {
+                if (!sKey || !this.hasOwnProperty(sKey)) { return; }
+                document.cookie = escape(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+                this.length--;
+            },
+            hasOwnProperty: function (sKey) {
+                return (new RegExp("(?:^|;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+            }
+        };
+        window.localStorage.length = (document.cookie.match(/\=/g) || window.localStorage).length;
+    }
+    /**
+     * data实现节点绑定数据  works in all browsers
+     * @namespace Cora.data
+     */
+    Cora.store = (function(){
+        var storage = window.localStorage;
+
+        var parseval = function(val){
+            if (typeof val !== 'string') { return undefined ;}
+            try { return JSON.parse(val) ;}
+            catch(e) { return val || undefined ;}
+        };
+
+        var strval = function(val){
+            return val === void 0 || typeof val === "function" ? val+'' : JSON.stringify(val);
+        };
+
+        var bindtime = function(val, expire){
+            if(expire===void 0)
+                return strval({"value":val});
+            return strval({
+                "value": val,
+                "expire": +new Date()+expire
+            });
+        };
+
+        var store = {
+            on: function(callback){
+                window.addEventListener('storage', function(e) {
+                   callback(e);
+                });
+            },
+            each: function(callback){
+                var that = this;
+                for(var i=0; i<storage.length; i++){
+                    key = storage.key[i];
+                    val =  that.noexpire(key);
+                    if(val){
+                        callback(key, val);
+                    }
+                }
+            },
+            /**
+             * 判断key是否过期，没有返回key的值，过期返回false
+             * @method Cora.store.noexpire
+             * @param {string} key
+             * @return noexpire
+             */
+            noexpire: function(key){
+                if(key===void 0){
+                    return false;
+                }
+                var that = this,
+                    now = new Date().getTime(),
+                    data = parseval(storage.getItem(key));
+                if(data.expire===void 0 || data.expire>now){
+                    return data.value;
+                }else{
+                    that.del(key);
+                    return false;
+                }
+            },
+            get: function(key){
+                if(key){
+                    return this.noexpire(key);
+                }
+                var ret = {};
+                this.each(function(key, val){
+                    ret[key] = val;
+                });
+                return ret;
+            },
+            /**
+             * 输出Debug执行时cpu信息, 在chrome profile面板查看相应详情
+             * @method Cora.store.set
+             * @param {string} key
+             * @param {object} val OR(string)
+             * @param {expire} expire 过期时间，此刻起的millsecond数 1000*60=1min后过期
+             */
+            set: function(key, val, expire){
+                if(typeof key === 'string'){
+                    storage.setItem(key, bindtime(val, expire));
+                    return this;
+                }
+                var data = key;
+                for (var k in data) {
+                    val = data[k];
+                    this.set(k, val, expire);
+                }
+            },
+            del: function(key){
+                storage.removeItem(key);
+                return this;
+            },
+            cls:function(){
+                storage.clear();
+                return this;
+            },
+            has: function(key){
+                return storage.hasOwnProperty(key);
+            },
+            size: function(unit){
+                var bytes = JSON.stringify(localStorage).length
+                if(!unit)
+                    return bytes;
+                if(unit==='KB')
+                    return bytes/1024;
+                if(unit==='MB')
+                    return bytes/1024/1024;
+                return bytes;
+            }
+        };
+        return store;
+    })();
 
     /************************************ Data ***************************************/
     /**
@@ -964,7 +1106,7 @@
         PubSub.prototype.send = function(name, data){
             // 有deliver时订阅者即执行
             this.pubs[name].forEach(function(sub){
-                sub(data, name);
+                sub(name, data);
             });
             return this;
         };
